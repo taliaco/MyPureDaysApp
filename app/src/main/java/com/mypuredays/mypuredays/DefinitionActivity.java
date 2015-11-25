3fac7044e99f8931afd9a59cb7640465d75e78f0
yaelmo <yaelmoshko@gmail.com>
2015-11-25 11:35:14 +0200

    definition page

--- app/src/main/java/com/mypuredays/mypuredays/DefinitionActivity.java
+++ app/src/main/java/com/mypuredays/mypuredays/DefinitionActivity.java
@@ -3,12 +3,37 @@ package com.mypuredays.mypuredays;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DefinitionActivity extends Activity {
+
+    private ListView listDef;
+    private ArrayList<String> defNameList;
+    private ArrayAdapter<String> adapter;
+
+
+    private Definition def;
+
+    private static int  NUM_OF_DEF=8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definition);
+
+        listDef= (ListView)findViewById(R.id.definition_list);
+
+        //def=
+
+        for(int i=0; i<NUM_OF_DEF; i++){
+
+            defNameList.add("1");
+        }
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, defNameList);
+        listDef.setAdapter(adapter);
     }
 }
