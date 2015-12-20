09f2d44da285f088c7de1470cf6c64c7bcd3241a
brahafa <brachafab@gmail.com>
2015-12-20 12:23:27 +0200

    change name button2

--- app/src/main/java/com/mypuredays/mypuredays/BL.java
+++ app/src/main/java/com/mypuredays/mypuredays/BL.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.Locale;
 
 /**
- * Created by Talia on 25/11/2015.
+ * Created by Talia on 25/11/2015. apppppppppppppppppppppppppppppp
  */
 public class BL {
     DAL dal;
